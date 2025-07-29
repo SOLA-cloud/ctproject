b109187da61591befb66ab6d104c8e95a80a92c0
SOLA-cloud <sora.jenny.5@gmail.com>
2025-07-29 15:21:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as htmlviewer
 st.set_page_config(layout='wide', page_title='DNA sequencying!!!')
 
 #Title Msg#1
-st.title("This is my webapp!!")
+st.title("This is my CT project!")
 
 with open('./index.html', 'r', encoding='utf-8') as f:
     html = f.read()
@@ -25,25 +25,26 @@ with open('./index.html', 'r', encoding='utf-8') as f:
 # Box#1(4),Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with st.expander('Content #1...'):
-        url = 'https://youtu.be/XyEOEBsa8I4?si=uBEbdWUE46feAOO-'
-        st.info('Content..')
-        st.video(url)
+    with col1:
+        with st.expander('AI applied CT #1'):
+            with open('./CT.txt', 'r', encoding='utf-8') as f:
+                text = f.read()
+                st.text_area("텍스트 내용", text, height=600)
 
-    with st.expander('Image Content #1...'):
+    with st.expander('Bebras Image Content #1...'):
         imgfile = './images/bebras(1).jpg'
         st.image(imgfile)
 
-    with st.expander('Image Content #2...'):
+    with st.expander('Bebras Image Content #2...'):
         imgfile = './images/bebras(2).jpg'
         st.image(imgfile)
 
-    with st.expander('Content #2...'):
+    with st.expander('AI-X Content'):
         htmlviewer.html(html, height=2300)
         
 with col2:
     with st.expander('Tips..'):
-        st.info('Tips..')
+        st.info('1) AI applied CT\n2) Bebras Image\n3) AI-X Content')
 
 st.markdown('<hr>', unsafe_allow_html=True)
-st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
+st.write('<font color="BLUE">(c)copyright. all rights reserved by SOLA5', unsafe_allow_html=True)
