ae1ecc83b6dbbc89a8afbfee38ecaeec8552f39a
SOLA-cloud <sora.jenny.5@gmail.com>
2025-07-31 19:36:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
-st.set_page_config(layout='wide', page_title='DNA sequencying!!!')
+st.set_page_config(layout='wide', page_title='Click-Through Rate')
 
 #Title Msg#1
 st.title("This is my CT project!")
@@ -10,6 +10,7 @@ with open('./index.html', 'r', encoding='utf-8') as f:
     f.close()
 
 #html = '''
+
 #<html>
 #    <head>
 #       <title> this is my html </title>
@@ -25,26 +26,31 @@ with open('./index.html', 'r', encoding='utf-8') as f:
 # Box#1(4),Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with col1:
-        with st.expander('AI applied CT #1'):
-            with open('./CT.txt', 'r', encoding='utf-8') as f:
-                text = f.read()
-                st.text_area("텍스트 내용", text, height=600)
+        import streamlit as st
+        import streamlit.components.v1 as htmlviewer
 
-    with st.expander('Bebras Image Content #1...'):
-        imgfile = './images/bebras(1).jpg'
-        st.image(imgfile)
+        with open('./loan_optimization.html', 'r', encoding='utf-8') as f:
+            CT_html = f.read()
 
-    with st.expander('Bebras Image Content #2...'):
-        imgfile = './images/bebras(2).jpg'
-        st.image(imgfile)
+        with st.expander('CT Content #1'):
+            with open("loan_optimization.html", "r", encoding="utf-8") as f:
+                html_code = f.read()
+            htmlviewer.html(html_code, height=1000, scrolling=True)
 
-    with st.expander('AI-X Content'):
-        htmlviewer.html(html, height=2300)
+
+        with st.expander('CT Content #2'):
+            with open("saving_plan.html", "r", encoding="utf-8") as f:
+                html_code = f.read()
+            htmlviewer.html(html_code, height=1000, scrolling=True)  
+ 
+        with st.expander('AI-X Content'):
+            with open("index.html", "r", encoding="utf-8") as f:
+                html_code = f.read()
+            htmlviewer.html(html_code, height=2300)
         
 with col2:
     with st.expander('Tips..'):
-        st.info('1) AI applied CT\n2) Bebras Image\n3) AI-X Content')
+        st.info('1) CT content #1\n2) CT content #2\n3) AI-X Content')
 
 st.markdown('<hr>', unsafe_allow_html=True)
 st.write('<font color="BLUE">(c)copyright. all rights reserved by SOLA5', unsafe_allow_html=True)
